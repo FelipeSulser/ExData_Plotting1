c9c91754ca7244fd96422b9566f9234efc698318
Felipe Sulser <felipesulser@gmail.com>
2016-06-11 11:23:17 +0200

    Added content
    Done

---
+++ plot2.R
@@ -0,0 +1,9 @@
+#plot 2 -> global active power between 1-2-2007 and 2-2-2007 as plot
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
